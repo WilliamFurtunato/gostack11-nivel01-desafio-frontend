39b96d81a41fbc5f93626fa4665c2c197c9643b2
William Furtunato <williamfurtunato@hotmail.com>
2020-04-15 20:18:18 -0300

    features

---
+++ README.md
@@ -0,0 +1,42 @@
+![GoStack](https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png)
+
+# Desafio 03: Conceitos do ReactJS
+
+[Sobre o desafio ](#sobre-o-desafio) |
+[Sobre a aplicação](#sobre-a-aplica%c3%a7%c3%a3o) |
+[Teste você mesmo](#teste-voc%c3%aa-mesmo) |
+[Licença](#licen%c3%a7a) 
+
+## Sobre o desafio
+ 
+ Criar uma aplicação web para treinar conceitos abordados com ReactJS, como: 
+ - Components
+ - Propriedades
+ - Estado & Imutabilidade
+ - Consumo de API's.
+
+## Sobre a aplicação
+
+Essa será uma aplicação para consumir dados de um backend, com isso, listar os repositórios, adicionar um repositório e remover um repositório. 
+
+ 
+## Teste você mesmo 
+
+Você pode encontrar o backend da aplicação aqui: [API Node.JS](https://github.com/WilliamFurtunato/gostack11-nivel01-desafio-backend.git). 
+
+Lembre-se de seguir as instruções de inicialização do backend.
+
+Para testar essa aplicação em sua máquina, basta realizar o seguinte: 
+
+- Clone esse repositório usando: 
+ 
+  `git clone https://github.com/WilliamFurtunato/gostack11-nivel01-desafio-frontend.git`
+
+- Navegue até a pasta criada e abra no Visual Studio Code, lembre-se de executar o comando `yarn` no seu terminal para instalar todas as dependências
+
+- Execute o comando `yarn start` no seu terminal para iniciar a aplicação.
+
+## Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import './styles.css';
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: 'Front-end com ReactJS',
+      url: 'https://github.com/WilliamFurtunato/be-the-hero',
+      techs: ['JavaScript', 'ReactJS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
+
+    if (response.status === 204) {
+      const newRepositories = repositories.filter(
+        repository => repository.id !== id
+      );
+
+      setRepositories(newRepositories);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
